e8f30d4680d823b6858fa3e6ebfcdbdbe2c21fe3
Tommy Nguyen <tommylulxdd@gmail.com>
2023-11-28 23:57:21 -0800

    Added Opportunity page and updated recommendation page

--- app/page.js
+++ app/page.js
@@ -1,6 +1,7 @@
 import Image from "next/image";
 import styles from "./page.module.css";
 import Recommendations from "./Recommendation";
+import Opportunity from "./opportunity/page";
 import { VStack, Center } from "@chakra-ui/layout";
 
 export default function Home() {
